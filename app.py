1cfac0dfd5a3e9fd41617421b3a2d6104d164942
geoflegrand <gessongue@gmail.com>
2024-07-31 19:06:26 +0100

    update
    Signed-off-by: geoflegrand <gessongue@gmail.com>

--- app.py
+++ app.py
@@ -1,19 +1,19 @@
 import sys
-import json
+#import json
 from pprint import pprint
 import yaml
-from apis import jen
-from csv_file import csvgeneretor as c
+#from apis import jen
+#from csv_file import csvgeneretor as c
 from yaml_file import manageyaml as y
 
 
 # print(sys.platform)
-def data_jenkins():
-    file_name = "jenkins_jobs.csv"
-    header = ['Job_name', 'Job_url', 'Job_color']
-    keys=['name','color','url']
-    content = jen.my_jobs(keys)
-    c.csv_generator(file_name,header,content)
+# def data_jenkins():
+#     file_name = "jenkins_jobs.csv"
+#     header = ['Job_name', 'Job_url', 'Job_color']
+#     keys=['name','color','url']
+#     content = jen.my_jobs(keys)
+#     c.csv_generator(file_name,header,content)
 
 
 def update_yaml_file(_file,save_file):
