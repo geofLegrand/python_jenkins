397195a306cfd7a860f4f0001f3ce9070f9dece1
geoflegrand <gessongue@gmail.com>
2024-07-31 21:40:48 +0100

    update
    Signed-off-by: geoflegrand <gessongue@gmail.com>

--- yaml_file/manageyaml.py
+++ yaml_file/manageyaml.py
@@ -5,7 +5,7 @@ import yaml
 def red_yaml_file(yaml_file):
     _yaml_file = {}
     try:
-        with open(yaml_file,'r',newline='') as yaml_doc:
+        with open(yaml_file,'r') as yaml_doc:
             _yaml_file = yaml.load(yaml_doc,Loader=yaml.FullLoader)
         return _yaml_file
     except Exception as e:
